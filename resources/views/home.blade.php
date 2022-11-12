9ef43c2f80f368ab069a8ffd513d4e0340ad059b
Cesar Alvarez <cesar_zuco@hotmail.com>
2022-11-12 15:04:18 -0600

    BootForm class added and form image finished

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="page-inner my-5">
         <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
             <div>
@@ -10,5 +9,42 @@
             </div>
         </div>
     </div>
+    <div class="content">
+        <div class="col-md-12 my-3">
+            <div class="card full-height">
+                <div class="card-header">
+                    <div class="card-head-row">
+                        <div class="card-title">Encuentra tu actuvidad preferida</div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    {!! BootForm::open() !!}
+                    <div class="row justify-content-md-center">
+                        <div class="col col-lg-2">
+                            <div class="form-group">
+                                <label class="control-label" for="fecha">Fecha de la reservación</label>
+                                <input type="date" name="fecha" {{-- onchange="validarCamposBusqueda(this)" --}} id="fecha"
+                                    class="form-control">
+                            </div>
+                        </div>
+                        <div class="col col-lg-2">
+                            <div class="form-group">
+                                <label class="control-label" for="personas">Total de personas a asistir</label>
+                                <input type="number" name="personas" {{-- onchange="validarCamposBusqueda(this)" --}} id="personas"
+                                    class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-md-center">
+                        <div class="col-md-auto">
+                            <button class="btn btn-success">Submit</button>
+                        </div>
+                    </div>
 
+                    {!! BootForm::close() !!}
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
