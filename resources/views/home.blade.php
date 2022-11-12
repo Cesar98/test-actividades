d934d81451ea76f6804ec945e4aef1bc811357d8
Cesar Alvarez <cesar_zuco@hotmail.com>
2022-11-12 14:34:46 -0600

    Atlantis dashboard implementation to system

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="page-inner my-5">
+        <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
+            <div>
+                <h2 class="text-white pb-2 fw-bold">Bienvenido</h2>
+                <h5 class="text-white op-7 mb-2">Reserva de actividades en Toluca Edo. Mex.</h5>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Top actividades en Toluca</title>
+    <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+    <link rel="icon" href="{{ asset('assets/img/icon.ico') }}" type="image/x-icon" />
+
+    <!-- Fonts and icons -->
+    <script src="{{ asset('assets/js/plugin/webfont/webfont.min.js') }}"></script>
+    <script>
+        WebFont.load({
+            google: {
+                "families": ["Lato:300,400,700,900"]
+            },
+            custom: {
+                "families": ["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands",
+                    "simple-line-icons"
+                ],
+                urls: ["{{ asset('assets/css/fonts.min.css') }}"]
+            },
+            active: function() {
+                sessionStorage.fonts = true;
+            }
+        });
+    </script>
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/atlantis.min.css') }}">
+
+</head>
+
+<body data-background-color="dark">
+    <div class="wrapper">
+        <div class="main-header">
+            <!-- Logo Header -->
+            <div class="logo-header" data-background-color="dark2">
+
+                <a href="https://themekita.com/demo-atlantis-lite-bootstrap/#" class="logo">
+                    <img src="{{ asset('assets/img/logo.svg') }}" alt="Atlantis lite logo" class="navbar-brand">
+                </a>
+                <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse"
+                    data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon">
+                        <i class="icon-menu"></i>
+                    </span>
+                </button>
+                <button class="topbar-toggler more"><i class="icon-options-vertical"></i></button>
+                <div class="nav-toggle">
+                    <button class="btn btn-toggle toggle-sidebar">
+                        <i class="icon-menu"></i>
+                    </button>
+                </div>
+            </div>
+            <!-- End Logo Header -->
+
+            <!-- Navbar Header -->
+            <nav class="navbar navbar-header navbar-expand-lg" data-background-color="dark">
+            </nav>
+            <!-- End Navbar -->
+        </div>
+
+        <!-- Sidebar -->
+        <div class="sidebar sidebar-style-2" data-background-color="dark2">
+            <div class="sidebar-wrapper scrollbar scrollbar-inner">
+                <div class="sidebar-content">
+                    <ul class="nav nav-primary">
+                        <li class="nav-item @if (str_contains(Route::current()->getName(), 'actividades')) active @endif">
+                            <a href="{{-- {{ route('direccion.index') }} --}}">
+                                <i class="fas fa-child"></i>
+                                <p>Actividades</p>
+                            </a>
+                        </li>
+                        <li class="nav-item @if (str_contains(Route::current()->getName(), 'reservaciones')) active @endif">
+                            <a href="{{-- {{ route('busqueda.index') }} --}}">
+                                <i class="fas fa-th-list"></i>
+                                <p>Reservaciónes</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- End Sidebar -->
+
+        <div class="main-panel">
+            @yield('content')
+        </div>
+
+    </div>
+
+
+    <script src="{{ asset('assets/js/core/jquery.3.2.1.min.js') }}"></script>
+	<script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+	<script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+
+	<!-- jQuery UI -->
+	<script src="{{ asset('assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
+	<script src="{{ asset('assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js') }}"></script>
+
+	<!-- jQuery Scrollbar -->
+	<script src="{{ asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
+
+	<!-- Chart JS -->
+	<script src="{{ asset('assets/js/plugin/chart.js/chart.min.js') }}"></script>
+
+	<!-- jQuery Sparkline -->
+	<script src="{{ asset('assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js') }}"></script>
+
+	<!-- Chart Circle -->
+	<script src="{{ asset('assets/js/plugin/chart-circle/circles.min.js') }}"></script>
+
+	<!-- Datatables -->
+	<script src="{{ asset('assets/js/plugin/datatables/datatables.min.js') }}"></script>
+
+	<!-- Bootstrap Notify -->
+	<script src="{{ asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
+
+	<!-- Sweet Alert -->
+	<script src="{{ asset('assets/js/plugin/sweetalert/sweetalert.min.js') }}"></script>
+
+	<!-- Atlantis JS -->
+	<script src="{{ asset('assets/js/atlantis.min.js') }}"></script>
+
+
+    @yield('script')
+
+</body>
+
+</html>
