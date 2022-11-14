2cf8a935d5de1645fa081bcac163ba8e6f632439
Cesar Alvarez <cesar_zuco@hotmail.com>
2022-11-14 01:01:34 -0600

    Heroku details

--- app/Http/Controllers/Api/ActividadesController.php
+++ app/Http/Controllers/Api/ActividadesController.php
@@ -21,7 +21,7 @@ class ActividadesController extends Controller
 
         $actividad = Actividad::find($request->actividad_id);
 
-        $actividades_relacionadas = Actividad::whereIn('id', json_decode($actividad->actividades_relacionadas));
+        $actividades_relacionadas = Actividad::whereIn('id', array($actividad->actividades_relacionadas));
 
         return response()->json([
             "actividad" => $actividad,
