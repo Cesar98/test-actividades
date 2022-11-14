0ec8b5709343ac000985517b52418607abdf9c4b
Cesar Alvarez <cesar_zuco@hotmail.com>
2022-11-14 00:39:07 -0600

    Heroku details

--- app/Http/Controllers/Api/ActividadesController.php
+++ app/Http/Controllers/Api/ActividadesController.php
@@ -20,11 +20,11 @@ class ActividadesController extends Controller
     public function detalle (Request $request){
 
         $actividad = Actividad::find($request->actividad_id);
-        /* $actividades_relacionadas = Actividad::find(array($actividad->actividades_relacionadas)); */
+        $actividades_relacionadas = Actividad::whereIn("id", json_decode($actividad->actividades_relacionadas));
 
         return response()->json([
             "actividad" => $actividad,
-            "actividades_relacionadas" => gettype(json_decode($actividad->actividades_relacionadas))/* $actividades_relacionadas */
+            "actividades_relacionadas" => $actividades_relacionadas->get()
         ]);
 
     }
